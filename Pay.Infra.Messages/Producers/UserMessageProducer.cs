06dab2b9c7bf4f1272ea6ea4c4c0000a6ca92f2e
mariohenriqueoliveiranascimento <mario5.8.2.8.10@hotmail.com>
2024-01-22 02:24:47 -0300

    Adicionar arquivos de projeto.

---
+++ Pay.Infra.Messages/Producers/UserMessageProducer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Pay.Domain.Interfaces.Messages;
+using Pay.Domain.ValueObjects;
+using Pay.Infra.Messages.Settings;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pay.Infra.Messages.Producers
+{
+    public class UserMessageProducer : IUserMessageProducer
+    {
+        private readonly RabbitMQSettings? _rabbitMQSettings;
+        public UserMessageProducer(IOptions<RabbitMQSettings?> rabbitMQSettings)
+        {
+            _rabbitMQSettings = rabbitMQSettings.Value;
+        }
+
+        public void Send(UserMessageVO userMessage)
+        {
+            var connectionFactory = new ConnectionFactory()
+            { Uri = new Uri(_rabbitMQSettings?.Url) };
+
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var model = connection.CreateModel())
+                {
+                    //criando a fila
+                    model.QueueDeclare(
+                        queue: _rabbitMQSettings.Queue, //nome da fila
+                        durable: true,
+                        //não apagar as filas ao desligar
+                        //ou reiniciar o broker
+                        autoDelete: false, //apagar ou não a fila quando
+                                           //ela estiver sem mensagens (vazia)
+                        exclusive: false,
+                        //fila exclusiva para uma unica aplicaçao ou não
+                        arguments: null
+                        );
+
+                    //gravando mensagem na fila
+                    model.BasicPublish(
+                        exchange: string.Empty,
+                        routingKey: _rabbitMQSettings.Queue,
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes
+                        (JsonConvert.SerializeObject(userMessage))
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ Pay.Tests/PaymentSlipAppServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AutoMapper;
+using Moq;
+using Pay.Application.Dtos.Requests;
+using Pay.Application.Dtos.Responses;
+using Pay.Application.Services;
+using Pay.Domain.Interfaces.Services;
+using Pay.Domain.Moldes;
+using Xunit;
+
+namespace Pay.Tests
+{
+    public class PaymentSlipAppServiceTest
+    {
+        [Fact]
+        public void Create_DeveChamarMetodoCreateDoDomainServiceEMappearParaDto()
+        {
+            // Arrange
+            var paymentSlipDomainServiceMock = new Mock<IPaymentSlipDomainService>();
+            var mapperMock = new Mock<IMapper>();
+
+            var paymentSlipAppService = new PaymentSlipAppService(paymentSlipDomainServiceMock.Object, mapperMock.Object);
+
+            var paymentSlipAddRequestDto = new PaymentSlipAddRequestDto
+            {
+                PayerName = "John Doe",
+                // Outras propriedades necessárias para criar um PaymentSlipAddRequestDto
+            };
+
+            var paymentSlip = new PaymentSlip
+            {
+                Id = Guid.NewGuid(),
+                PayerName = paymentSlipAddRequestDto.PayerName,
+                // Outras propriedades necessárias para criar um PaymentSlip
+            };
+
+            mapperMock.Setup(x => x.Map<PaymentSlipResponseDto>(paymentSlip)).Returns(new PaymentSlipResponseDto());
+
+            // Act
+            var result = paymentSlipAppService.Create(paymentSlipAddRequestDto);
+
+            // Assert
+            paymentSlipDomainServiceMock.Verify(x => x.Create(It.IsAny<PaymentSlip>()), Times.Once);
+            mapperMock.Verify(x => x.Map<PaymentSlipResponseDto>(paymentSlip), Times.Once);
+
+            // Adicione mais verificações conforme necessário para garantir que o método está se comportando conforme esperado.
+        }
+    }
+
+}
